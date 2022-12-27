ff21a0e15f8a9824093e9604111412e2dfcdc082
jvalencia2001 <jvalencia.work@gmail.com>
2022-12-26 22:26:19 -0500

    Created endpoints to get single collec and card.

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -8,7 +8,7 @@ import Card from "./models/cardModel";
 const app = express();
 app.use(express.json());
 
-app.post("/create-user", (req, res) => {
+app.post("/newUser", (req, res) => {
   const newUser = new User(
     req.body.uuid,
     req.body.displayName,
@@ -28,7 +28,7 @@ app.post("/create-user", (req, res) => {
     });
 });
 
-app.post("/create-collection", (req, res) => {
+app.post("/newCollection", (req, res) => {
   const newCollection = new Collection(
     req.body.name,
     req.body.userID,
@@ -57,7 +57,7 @@ app.post("/create-collection", (req, res) => {
     });
 });
 
-app.post("/create-card", (req, res) => {
+app.post("/newCard", (req, res) => {
   const newCard = new Card(
     req.body.name,
     req.body.content,
@@ -92,7 +92,7 @@ type responseContent = {
   objects: Array<{}>;
 };
 
-app.get("/getInformationFromUser/:userID", (req, res) => {
+app.get("/user/:userID", (req, res) => {
   let response: responseContent = { objects: [] };
 
   const query = db.collection("users").where("id", "==", req.params.userID);
@@ -110,7 +110,7 @@ app.get("/getInformationFromUser/:userID", (req, res) => {
     });
 });
 
-app.get("/getCollectionsFromUser/:userID", (req, res) => {
+app.get("/user/:userID/collections", (req, res) => {
   let response: responseContent = { objects: [] };
 
   const query = db
@@ -130,12 +130,50 @@ app.get("/getCollectionsFromUser/:userID", (req, res) => {
     });
 });
 
-app.get("/getCardsFromCollection/:collectionID", (req, res) => {
+app.get("/collections/:collectionID", (req, res) => {
+  let response: responseContent = { objects: [] };
+
+  const query = db
+    .collection("collections")
+    .where("collectionID", "==", req.params.collectionID);
+
+  query
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.docs.forEach((doc) => {
+        response.objects.push(doc.data());
+      });
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Couldn't query the database." });
+    });
+});
+
+app.get("/collections/:collectionID/cards", (req, res) => {
   let response: responseContent = { objects: [] };
 
   const query = db
     .collection("cards")
     .where("collectionID", "==", req.params.collectionID);
+
+  query
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.docs.forEach((doc) => {
+        response.objects.push(doc.data());
+      });
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Couldn't query the database." });
+    });
+});
+
+app.get("/cards/:cardID", (req, res) => {
+  let response: responseContent = { objects: [] };
+
+  const query = db.collection("cards").where("cardID", "==", req.params.cardID);
 
   query
     .get()
