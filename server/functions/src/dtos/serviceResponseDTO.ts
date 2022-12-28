09a61dbc9e4a960ae3510fca5d905551e6555fee
jvalencia2001 <jvalencia.work@gmail.com>
2022-12-28 15:51:39 -0500

    Changed the type of serviceData.

--- server/functions/src/dtos/serviceResponseDTO.ts
+++ server/functions/src/dtos/serviceResponseDTO.ts
@@ -1,11 +1,11 @@
 export class ServiceResponse<T> {
-  _serviceName: string;
-  _serviceData?: Array<T>;
-  _serviceMessage: string;
+  serviceName: string;
+  serviceData?: T;
+  serviceMessage: string;
 
-  constructor(name: string, message: string, data?: Array<T>) {
-    this._serviceName = name;
-    this._serviceMessage = message;
-    this._serviceData = data;
+  constructor(name: string, message: string, data?: T) {
+    this.serviceName = name;
+    this.serviceMessage = message;
+    this.serviceData = data;
   }
 }
